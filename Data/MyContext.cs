fa090ce9ab9d33b5bf7360a754153b6c3d409f1d
alice <alice@ALICE-PC>
2025-01-21 00:05:28 +0800

    加入專案檔案。

---
+++ Data/MyContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCpractice.Models;
+
+namespace MVCpractice.Data
+{//proprate and mathed / context to project and database.
+    public class MyContext :DbContext
+    {
+        
+        public MyContext(DbContextOptions<MyContext>options):base(options) { }
+        public DbSet<Item>items { get; set; }
+    }
+}
